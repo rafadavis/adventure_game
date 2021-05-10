bfd17b62b72e340e758c0a5de7f70ad242cd56d3
Rafael Davis Portela <rdavisportela@gradcenter.cuny.edu>
2021-05-10 09:10:14 -0400

    first commit

---
+++ game.py
@@ -0,0 +1,34 @@
+from player import Player
+import world
+
+def play():
+    print("Escape from Cave Terror!")
+    player = Player()
+    
+    while True:
+        room = world.tile_at(player.x, player.y)
+        print(room.intro_text())
+        action_input = get_player_command()
+
+        if action_input == 'n':
+            player.move_north()
+
+        elif action_input == 's':
+            player.move_south()
+
+        elif action_input == 'e':
+            player.move_east()
+
+        elif action_input == 'w':
+            player.move_west()
+
+        elif action_input == 'i':
+            player.print_inventory()
+            
+        else:
+            print("Invalid action!")
+
+def get_player_command():
+    return input('Action: ').lower()
+
+play()
